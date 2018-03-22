ccbc9219bda554e11dfd2d06f515f50c52ff3da4
RuiDTLima <rui_lima24@hotmail.com>
2018-03-22 10:23:52 +0000

    Add first draft of Spring implementation

---
+++ API/ChecklistAPI/src/main/java/pt/isel/daw/g5/ChecklistAPI/ChecklistApiApplication.java
@@ -0,0 +1,12 @@
+package pt.isel.daw.g5.ChecklistAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChecklistApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChecklistApiApplication.class, args);
+	}
+}
---
+++ API/ChecklistAPI/src/main/java/pt/isel/daw/g5/ChecklistAPI/model/User.java
@@ -0,0 +1,6 @@
+package pt.isel.daw.g5.ChecklistAPI.model;
+
+public class User {
+
+    public
+}
