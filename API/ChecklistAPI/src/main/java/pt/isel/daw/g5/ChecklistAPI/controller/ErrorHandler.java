487d1628d4038adb88ec0a7dd34fe87e215806d3
danielagomes <42799@alunos.isel.ipl.pt>
2018-04-02 13:07:55 +0100

    adds error handler for unauthorized exception

--- API/ChecklistAPI/src/main/java/pt/isel/daw/g5/ChecklistAPI/controller/ErrorHandler.java
+++ API/ChecklistAPI/src/main/java/pt/isel/daw/g5/ChecklistAPI/controller/ErrorHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import pt.isel.daw.g5.ChecklistAPI.exceptions.InvalidStateException;
 import pt.isel.daw.g5.ChecklistAPI.exceptions.ForbiddenException;
+import pt.isel.daw.g5.ChecklistAPI.exceptions.UnauthorizedException;
 import pt.isel.daw.g5.ChecklistAPI.model.errorModel.ProblemJSON;
 
 @ControllerAdvice
@@ -22,5 +23,10 @@ public class ErrorHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(ForbiddenException.class)
     public ResponseEntity<ProblemJSON> notAuthenticated(ForbiddenException ex){
         return new ResponseEntity<>(ex.getProblemJSON(), HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<ProblemJSON> notAuthenticated(UnauthorizedException ex){
+        return new ResponseEntity<>(ex.getProblemJSON(), HttpStatus.UNAUTHORIZED);
     }
 }
